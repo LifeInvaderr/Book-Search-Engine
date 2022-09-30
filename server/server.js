6290c97f708806c9a2e64a97fb3c8fee4ba6926b
Matthew Kaus <matthewkausfox@gmail.com>
2022-09-30 06:29:44 -0600

    fixed server apollo v2 to apollo v3

--- server/server.js
+++ server/server.js
@@ -1,36 +1,41 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
-// import our TypeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas')
 const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
 
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
 });
 
-// integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-db.once('open', () => {
-  app.listen(PORT, () => 
-  console.log(`🌍 Now listening on http://localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running port ${PORT}!`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
+
+startApolloServer(typeDefs, resolvers);
